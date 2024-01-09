2a813c93c586ede9049a4c8caa09891fe05699e1
Sweeze <brandonsweezer@gmail.com>
2024-01-09 13:14:39 -0800

    google stun servers

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,89 @@
 "use client";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default async function Home() {
-  const videos = useRef<HTMLVideoElement>(null);
+export default function Home() {
+  const [connection, setConnection] = useState<RTCPeerConnection>();
+  const [localStream, setLocalStream] = useState<MediaStream>();
+  const [remoteStream, setRemoteStream] = useState<MediaStream>();
 
-  const startStream = async () => {
-    if (!videos.current) return;
-    const localStream = await navigator.mediaDevices.getDisplayMedia({video: true, audio: true});
-    videos.current.srcObject = localStream;
-    videos.current.onloadedmetadata = (e) => {
-      if (!videos.current) return;
-      videos.current.play();
-    }
+
+  const localVideo = useRef<HTMLVideoElement>(null);
+  const remoteVideo = useRef<HTMLVideoElement>(null);
+
+  const startStream = () => {
+    navigator.mediaDevices.getDisplayMedia({video: true, audio: true}).then((lStream) => {
+      setLocalStream(lStream);
+      if (!connection) return;
+      if (!localVideo.current) return;
+      localVideo.current.srcObject = lStream;
+
+      const newConnection = connection;
+      lStream.getTracks().forEach((track) => {
+        newConnection.addTrack(track, lStream);
+      })
+  
+      newConnection.ontrack = event => {
+        console.log('got track')
+        event.streams[0].getTracks().forEach(track => {
+          console.log('each track')
+          if (!remoteStream) return;
+          console.log('remote stream exists')
+          remoteStream.addTrack(track);
+          if (!remoteVideo.current) return;
+          console.log('remote video exists')
+          remoteVideo.current.srcObject = event.streams[0];
+        })
+      }
+
+      setConnection(newConnection);
+    });
+    
   };
+
+
+
+  const joinStream = () => {
+    console.log('joining stream')
+    if (!remoteVideo.current) return;
+    console.log('remote video exists');
+    if (!remoteStream) return;
+    console.log('remote stream exists');
+    remoteVideo.current.srcObject = remoteStream;
+  }
+
+  useEffect(() => {
+    const lStream = new MediaStream();
+    setLocalStream(lStream);
+    const rStream = new MediaStream();
+    setRemoteStream(rStream);
+    const config = {
+      iceServers: [
+        {
+          urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+        },
+      ],
+      iceCandidatePoolSize: 10,
+    };
+    const connection = new RTCPeerConnection(config);
+    connection.ontrack = (event) => {
+      
+    }
+    setConnection(connection);
+
+    
+    
+  }, [])
 
   return (
     <main>
-      <video ref={videos} autoPlay></video>
-      <button onClick={startStream}>Stream</button>
+      <div>
+        <video style={{width: 640, height: 360}} ref={localVideo} autoPlay></video>
+      </div>
+      <div>
+        <video style={{width: 640, height: 360, backgroundColor: "white"}} ref={remoteVideo} autoPlay></video>
+      </div>
+      <button style={{padding: 6, marginRight: 16, color: "burlywood"}} onClick={startStream}>Stream</button>
+      <button style={{padding: 6, marginRight: 16, color: "burlywood"}} onClick={joinStream}>Join</button>
     </main>
   )
 }
